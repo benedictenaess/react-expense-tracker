09d2afba557adabcb24ebefbeb3210286fc5f653
Benedicte Naess <144252770+benedictenaess@users.noreply.github.com>
2024-04-29 11:30:12 +0200

    Form add

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,33 @@ function App() {
 
   return (
     <>
-     
+      <form action="expense-tracker">
+        <div>
+          <label htmlFor="expense-title">Title</label>
+          <input type="text" />
+        </div>
+
+        <div>
+          <label htmlFor="expense-amount">Amount</label>
+          <input type="number" />
+        </div>
+
+        <div>
+          <label htmlFor="expense-date">Date</label>
+          <input type="date" />
+        </div>
+
+        <div>
+          <label htmlFor="select-category">Select category</label>
+          <select name="category" id="category">
+            <option value="default">--</option>
+            <option value="food">Food</option>
+            <option value="transportation">Transportation</option>
+            <option value="bills">Bills</option>
+            <option value="entertainment">Entertainment</option>
+          </select>
+        </div>
+      </form>
     </>
   )
 }
